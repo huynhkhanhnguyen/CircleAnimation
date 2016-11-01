d49f1b139ec1f2bf96667f46810c6b458a7aabd9
NguyenHuynh <nguyen07t1@gmail.com>
2016-11-01 23:53:51 +0700

    refactor 1

--- CircleAnimation/MonitorChart.swift
+++ CircleAnimation/MonitorChart.swift
@@ -29,19 +29,19 @@ private class MonitorChartLayer: CAShapeLayer {
 
 class MonitorChart: UIView {
   var circleLayer: CAShapeLayer!
-  var lineWidth = CGFloat(55)
+  var lineWidth = CGFloat(50)
   var animationDuration = 1.5 //secconds
 
   private var currentAnimationIndex = 0
   private var chartLayers: [MonitorChartLayer] = []
+  private var arcCenter = CGPointZero
+  private var radius: CGFloat = 0
   private let borderLayer = MonitorChartLayer()
 
   func setUpChartSegments(minValue: Double, maxValue: Double, segments: [MonitorChartSegment]) {
     chartLayers.removeAll()
-    let totalValueRange = maxValue - minValue
-    let lineWidth = CGFloat(55)
-    let arcCenter = CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0 + 1)
-    let radius = frame.size.width / 2
+    arcCenter = CGPoint(x: frame.size.width / 2.0, y: frame.size.height / 2.0 + 1)
+    radius = frame.size.width / 2
     let circlePath = UIBezierPath(arcCenter: arcCenter, radius: radius - lineWidth / 2, startAngle: CGFloat(-M_PI), endAngle: CGFloat(0), clockwise: true)
 
     for segment in segments {
@@ -53,35 +53,13 @@ class MonitorChart: UIView {
       circleLayer.strokeEnd = 0.0
       circleLayer.monitorChartSegment = segment
 
-      let animation = CABasicAnimation(keyPath: "strokeEnd")
-      animation.fromValue = (segment.startValue - minValue) / totalValueRange
-      animation.toValue = (segment.endValue - minValue) / totalValueRange
-      animation.duration = (segment.endValue - segment.startValue) / totalValueRange * animationDuration
-      animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-      animation.delegate = self
-      animation.removedOnCompletion = false
-      animation.fillMode = kCAFillModeBoth
+      let animation = createStrokeEndAnimationForSegment(segment, minValue: minValue, maxValue: maxValue)
       circleLayer.strokeStart = animation.fromValue as! CGFloat
       circleLayer.endPoint = animation.toValue as! CGFloat
       circleLayer.animation = animation
 
       chartLayers.append(circleLayer)
     }
-
-    let borderPath = UIBezierPath()
-    borderPath.moveToPoint(CGPoint(x: arcCenter.x - radius + lineWidth - 1, y: arcCenter.y))
-    borderPath.addArcWithCenter(CGPoint(x: arcCenter.x, y: arcCenter.y), radius: radius - lineWidth, startAngle: CGFloat(-M_PI), endAngle: 0, clockwise: true)
-    borderPath.addLineToPoint(CGPoint(x: arcCenter.x + radius, y: arcCenter.y))
-    borderPath.addArcWithCenter(CGPoint(x: arcCenter.x, y: arcCenter.y), radius: radius, startAngle: 0, endAngle: CGFloat(-M_PI), clockwise: false)
-    borderPath.addLineToPoint(CGPoint(x: arcCenter.x - radius + lineWidth - 1, y: arcCenter.y))
-    borderPath.closePath()
-
-    borderLayer.path = borderPath.CGPath
-    borderLayer.fillColor = UIColor.clearColor().CGColor
-    borderLayer.strokeColor = UIColor.grayColor().CGColor
-    borderLayer.lineWidth = 1
-    borderLayer.strokeEnd = 1.0
-    layer.addSublayer(borderLayer)
   }
 
   override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
@@ -92,6 +70,8 @@ class MonitorChart: UIView {
     currentAnimationIndex += 1
     if currentAnimationIndex < chartLayers.count {
       chartLayers[currentAnimationIndex].performAnimation()
+    } else {
+      addBorder()
     }
   }
 
@@ -108,5 +88,36 @@ class MonitorChart: UIView {
     }
     borderLayer.performAnimation()
     chartLayers.first?.performAnimation()
+  }
+
+  private func createStrokeEndAnimationForSegment(segment: MonitorChartSegment, minValue: Double, maxValue: Double) -> CABasicAnimation {
+    let totalValueRange = maxValue - minValue
+    let animation = CABasicAnimation(keyPath: "strokeEnd")
+    animation.fromValue = (segment.startValue - minValue) / totalValueRange
+    animation.toValue = (segment.endValue - minValue) / totalValueRange
+    animation.duration = (segment.endValue - segment.startValue) / totalValueRange * animationDuration
+    animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+    animation.delegate = self
+    animation.removedOnCompletion = false
+    animation.fillMode = kCAFillModeBoth
+
+    return animation
+  }
+
+  private func addBorder() {
+    let borderPath = UIBezierPath()
+    borderPath.moveToPoint(CGPoint(x: arcCenter.x - radius + lineWidth - 1, y: arcCenter.y))
+    borderPath.addArcWithCenter(CGPoint(x: arcCenter.x, y: arcCenter.y), radius: radius - lineWidth, startAngle: CGFloat(-M_PI), endAngle: 0, clockwise: true)
+    borderPath.addLineToPoint(CGPoint(x: arcCenter.x + radius, y: arcCenter.y))
+    borderPath.addArcWithCenter(CGPoint(x: arcCenter.x, y: arcCenter.y), radius: radius, startAngle: 0, endAngle: CGFloat(-M_PI), clockwise: false)
+    borderPath.addLineToPoint(CGPoint(x: arcCenter.x - radius + lineWidth - 1, y: arcCenter.y))
+    borderPath.closePath()
+
+    borderLayer.path = borderPath.CGPath
+    borderLayer.fillColor = UIColor.clearColor().CGColor
+    borderLayer.strokeColor = UIColor.grayColor().CGColor
+    borderLayer.lineWidth = 1
+    borderLayer.strokeEnd = 1.0
+    layer.addSublayer(borderLayer)
   }
 }
