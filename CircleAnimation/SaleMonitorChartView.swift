8a9db30011914dbd13344b37f76b9e4b0ea496f9
NguyenHuynh <nguyen07t1@gmail.com>
2016-11-02 23:28:25 +0700

    complete chart

--- CircleAnimation/SaleMonitorChartView.swift
+++ CircleAnimation/SaleMonitorChartView.swift
@@ -8,14 +8,49 @@
 
 import UIKit
 
+extension CALayer {
+  var borderUIColor: UIColor {
+    set {
+      self.borderColor = newValue.CGColor
+    }
+    get {
+      return UIColor(CGColor: self.borderColor!)
+    }
+  }
+}
+
+extension UIView {
+  class func initFromNib() -> UIView {
+    let mainBundle = NSBundle.mainBundle()
+    let className  = NSStringFromClass(self).componentsSeparatedByString(".").last ?? ""
+    if ( mainBundle.pathForResource(className, ofType: "nib") != nil ) {
+      let objects = mainBundle.loadNibNamed(className, owner: self, options: [:])
+      for object in objects {
+        if let view = object as? UIView {
+          return view
+        }
+      }
+    }
+    return UIView(frame: CGRectZero)
+  }
+}
+
 class SaleMonitorChartView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
+  @IBOutlet weak var chartTitleLabel: UILabel!
+  @IBOutlet weak var monitorChart: MonitorChart!
 
+  @IBOutlet weak var indicatorNameLabel: UILabel!
+  @IBOutlet weak var quotaLabel: UILabel!
+  @IBOutlet weak var actualLabel: UILabel!
+  @IBOutlet weak var forecastLabel: UILabel!
+  @IBOutlet weak var attainmentLabel: UILabel!
+  @IBOutlet weak var expectedAttainmentLabel: UILabel!
+
+  func updateWithActualValue(actual: Double, quota: Double, min: Double, max: Double, segments: [MonitorChartSegment]) {
+    monitorChart.setUpChartWithSegments(segments, actualValue: actual, minValue: min, maxValue: max, saleQuota: quota)
+    quotaLabel.text = "$\(quota)"
+    actualLabel.text = "$\(actual)"
+    monitorChart.drawWithAnimation()
+  }
 }
--- CircleAnimation/ViewController.swift
+++ CircleAnimation/ViewController.swift
@@ -10,30 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
   var chart: MonitorChart!
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+  var chartView = SaleMonitorChartView.initFromNib() as! SaleMonitorChartView
 
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated)
+    let monitorSegment1 = MonitorChartSegment(startValue: 0, endValue: 300, color: UIColor.redColor())
+    let monitorSegment2 = MonitorChartSegment(startValue: 300, endValue: 900, color: UIColor.yellowColor())
+    let monitorSegment3 = MonitorChartSegment(startValue: 900, endValue: 1000, color: UIColor.greenColor())
 
-    let width = CGFloat(300)
-    let height = width
-    let pos = CGPoint(x: 100, y: 400)
-    chart = MonitorChart(frame: CGRect(origin: pos, size: CGSize(width: width, height: height)))
-    let monitorSegment1 = MonitorChartSegment(startValue: 0, endValue: 450, color: UIColor.blueColor())
-    let monitorSegment2 = MonitorChartSegment(startValue: 450, endValue: 900, color: UIColor.redColor())
-    let monitorSegment3 = MonitorChartSegment(startValue: 900, endValue: 1800, color: UIColor.greenColor())
 
-    view.addSubview(chart)
-    chart.setUpChartWithSegments([monitorSegment1, monitorSegment2, monitorSegment3], actualValue: 300, minValue: 0, maxValue: 1800, saleQuota: 1500)
-    chart.drawWithAnimation()
+    chartView.updateWithActualValue(620, quota: 700, min: 0, max: 1000, segments: [monitorSegment1, monitorSegment2, monitorSegment3])
+    chartView.frame.origin = CGPoint(x: 30, y: 200)
+
+    view.addSubview(chartView)
+//    let width = CGFloat(300)
+//    let height = width
+//    let pos = CGPoint(x: 100, y: 400)
+////    chart = MonitorChart(frame: CGRect(origin: pos, size: CGSize(width: width, height: height)))
+//    let monitorSegment1 = MonitorChartSegment(startValue: 0, endValue: 450, color: UIColor.redColor())
+//    let monitorSegment2 = MonitorChartSegment(startValue: 450, endValue: 900, color: UIColor.yellowColor())
+//    let monitorSegment3 = MonitorChartSegment(startValue: 900, endValue: 1800, color: UIColor.greenColor())
+//
+//    view.addSubview(chart)
+//    chart.setUpChartWithSegments([monitorSegment1, monitorSegment2, monitorSegment3], actualValue: 300, minValue: 0, maxValue: 1800, saleQuota: 1500)
+//    chart.drawWithAnimation()
   }
 
   @IBAction func redrawButtonPressed(sender: AnyObject) {
-    chart.drawWithAnimation()
+    let monitorSegment1 = MonitorChartSegment(startValue: 0, endValue: 360, color: UIColor.redColor())
+    let monitorSegment2 = MonitorChartSegment(startValue: 360, endValue: 620, color: UIColor.yellowColor())
+    let monitorSegment3 = MonitorChartSegment(startValue: 620, endValue: 1000, color: UIColor.greenColor())
+
+    chartView.updateWithActualValue(200, quota: 400, min: 0, max: 1000, segments: [monitorSegment1, monitorSegment2, monitorSegment3])
   }
 }
 
