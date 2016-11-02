1249e78e53d35c2471ab0c7148bded4ec6c3df3f
NguyenHuynh <nguyen07t1@gmail.com>
2016-11-02 23:06:26 +0700

    UI finished

---
+++ CircleAnimation/SaleMonitorChartView.swift
@@ -0,0 +1,21 @@
+//
+//  SaleMonitorChartView.swift
+//  CircleAnimation
+//
+//  Created by Nguyen Huynh on 11/2/16.
+//  Copyright © 2016 nguyenh. All rights reserved.
+//
+
+import UIKit
+
+class SaleMonitorChartView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
